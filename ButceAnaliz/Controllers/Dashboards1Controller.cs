b032504aa39a662b348ad1c9ce3bec57f3abaab9
ONUR ÇAYLAK <onrcylk1993@gmail.com>
2022-02-04 14:46:52 +0300

    add DashboardView

--- ButceAnaliz/Controllers/Dashboards1Controller.cs
+++ ButceAnaliz/Controllers/Dashboards1Controller.cs
@@ -9,22 +9,22 @@ using ButceAnaliz.Models;
 
 namespace ButceAnaliz.Controllers
 {
-    public class DashboardsController : Controller
+    public class Dashboards1Controller : Controller
     {
         private readonly DataContext _context;
 
-        public DashboardsController(DataContext context)
+        public Dashboards1Controller(DataContext context)
         {
             _context = context;
         }
 
-        // GET: Dashboards
+        // GET: Dashboards1
         public async Task<IActionResult> Index()
         {
             return View(await _context.Dashboards.ToListAsync());
         }
 
-        // GET: Dashboards/Details/5
+        // GET: Dashboards1/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -42,22 +42,18 @@ namespace ButceAnaliz.Controllers
             return View(dashboard);
         }
 
-        // GET: Dashboards/Create
+        // GET: Dashboards1/Create
         public IActionResult Create()
         {
             return View();
         }
-        public async Task<IActionResult> CreateIncome()
-        {
-            return View(await _context.Dashboards.ToListAsync());
-        }
 
-        // POST: Dashboards/Create
+        // POST: Dashboards1/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,GelenToplamTutar,GidenToplamTutar")] Dashboard dashboard)
+        public async Task<IActionResult> Create([Bind("Id,GelenToplamTutar,GidenToplamTutar,ToplamTutar,KayitTarihi")] Dashboard dashboard)
         {
             if (ModelState.IsValid)
             {
@@ -68,31 +64,7 @@ namespace ButceAnaliz.Controllers
             return View(dashboard);
         }
 
-        [HttpPost]
-       
-        public async Task<IActionResult> CreateIncome(int gelenToplamTutar,int gidenToplamTutar,int toplamTutar)
-        {
-            var dashboardUser = _context.Users.FirstOrDefault(x => x.Email == User.Identity.Name);
-            Dashboard dashboard = new Dashboard
-            {
-                ToplamTutar = toplamTutar,
-                GelenToplamTutar = gelenToplamTutar,
-                GidenToplamTutar = gidenToplamTutar,
-                KayitTarihi = DateTime.Now,
-                User = dashboardUser,
-            };
-
-            if (ModelState.IsValid)
-            {
-                _context.Add(dashboard);
-                await _context.SaveChangesAsync();
-                return View(dashboard);
-                //return RedirectToAction(nameof(Index));
-            }
-            return View(dashboard);
-        }
-
-        // GET: Dashboards/Edit/5
+        // GET: Dashboards1/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -108,12 +80,12 @@ namespace ButceAnaliz.Controllers
             return View(dashboard);
         }
 
-        // POST: Dashboards/Edit/5
+        // POST: Dashboards1/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,GelenToplamTutar,GidenToplamTutar")] Dashboard dashboard)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,GelenToplamTutar,GidenToplamTutar,ToplamTutar,KayitTarihi")] Dashboard dashboard)
         {
             if (id != dashboard.Id)
             {
@@ -143,7 +115,7 @@ namespace ButceAnaliz.Controllers
             return View(dashboard);
         }
 
-        // GET: Dashboards/Delete/5
+        // GET: Dashboards1/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -161,7 +133,7 @@ namespace ButceAnaliz.Controllers
             return View(dashboard);
         }
 
-        // POST: Dashboards/Delete/5
+        // POST: Dashboards1/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
